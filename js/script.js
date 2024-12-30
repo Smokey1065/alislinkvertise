bd6ca548ee84da172d75618dc59e4f73e8f11e7d
Smokey1065 <hessorogan@gmail.com>
2024-12-30 02:42:28 +0100

    completely new ui

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function tiktok() {
+  document.location.href = "https://www.tiktok.com/@dr.flashy";
+}
+
+function youtube() {
+  document.location.href = "https://www.youtube.com/@yt.flashy";
+}
+
+function github() {
+  document.location.href = "https://github.com/Smokey1065";
+}
+
+function snapchat() {
+  document.location.href = "https://snapchat.com/t/V6qKzxYJ";
+}
